bfb4e05a5b9f1ac302d7d7f44273d502901edd63
Isaac Braun <isaacbraun2000@gmail.com>
2024-05-31 19:29:45 -0400

    init

---
+++ content/_index.md
@@ -0,0 +1,6 @@
++++
+title = 'Braun Bauen'
+description = 'Welcome to Braun Bauen, a software development...'
+date = 2024-05-31T18:47:34-04:00
+draft = false
++++
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./content/*.md", "./content/**/*.md", "./layouts/*.html", "./layouts/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
