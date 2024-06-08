e449e73586515dd27df1825696e001b1eb9d4f64
Isaac Braun <isaacbraun2000@gmail.com>
2024-06-08 00:01:45 -0400

    init header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./content/*.md", "./content/**/*.md", "./layouts/*.html", "./layouts/**/*.html", "./public/*.html", "./public/**/*.html"],
   theme: {
+    fontFamily: {
+      'sans': ["Space Grotesk", "sans-serif"],
+      'mono': ["Space Mono", "monospace"],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      slate: colors.slate,
+      green: {
+        200: "#edf8f3",
+        600: "#009f4f",
+      },
+    },
     extend: {},
   },
   plugins: [
