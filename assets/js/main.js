cc4daf8767edb78013a49dcba31adbdb0487c5be
Isaac Braun <isaacbraun2000@gmail.com>
2024-06-17 19:49:17 -0400

    chore: removed magic methods

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,4 @@
 import Alpine from '/js/alpinejs.min.js'
-
-Alpine.magic('clipboard', (el) => {
-  const spanElem = el.querySelector('span');
-  spanElem.dataset.message = spanElem.innerText;
-  spanElem.innerText = 'Copied!';
-  return message => navigator.clipboard.writeText(message);
-});
-
-Alpine.magic('tooltipReset', (el) => {
-  const spanElem = el.querySelector('span');
-  console.warn(spanElem, spanElem.dataset.message);
-  spanElem.innerText = spanElem.dataset.message;
-  return null;
-});
  
 window.Alpine = Alpine;
-window.Alpine.start();
+Alpine.start();
