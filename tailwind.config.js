161fd394066ad3da7a3521211680cf664ee06321
Isaac Braun <isaacbraun2000@gmail.com>
2024-06-21 19:59:11 -0400

    feat: added smoother shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,12 @@ module.exports = {
         600: "#009f4f",
       },
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'smooth20': '0px 0.6px 0.8px rgba(0, 0, 0, 0.014), 0px 1.3px 2px rgba(0, 0, 0, 0.02), 0px 2.5px 3.8px rgba(0, 0, 0, 0.025), 0px 4.5px 6.7px rgba(0, 0, 0, 0.03), 0px 8.4px 12.5px rgba(0, 0, 0, 0.036), 0px 20px 30px rgba(0, 0, 0, 0.05)',
+        'smooth40': '0px 1.1px 1.1px rgba(0, 0, 0, 0.014), 0px 2.7px 2.7px rgba(0, 0, 0, 0.02), 0px 5px 5px rgba(0, 0, 0, 0.025), 0px 8.9px 8.9px rgba(0, 0, 0, 0.03), 0px 16.7px 16.7px rgba(0, 0, 0, 0.036), 0px 40px 40px rgba(0, 0, 0, 0.05)',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
