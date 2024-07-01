27461ce99820be112516a1d3bf5e370ba017c615
Isaac Braun <isaacbraun2000@gmail.com>
2024-06-30 23:33:02 -0400

    added swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,26 @@
-import Alpine from '/js/alpinejs.min.js'
- 
-window.Alpine = Alpine;
+import Alpine from "js/alpinejs.min.js";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+/**
+ * Alpine magic function to create a swiper instance with passed element
+ * @param {HTMLElement} elem
+ * @returns {null}
+ */
+// Alpine.magic('initSwiper', () => elem => {
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 Alpine.start();
